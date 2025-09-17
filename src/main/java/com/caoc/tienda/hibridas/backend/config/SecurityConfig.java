db448aea5b694492ca1ec12161c725632b8f4a2a
Carlos Andres Obando Casiano <73761531+caobandoc@users.noreply.github.com>
2025-09-16 21:51:24 -0500

    Add new CORS origin for GitHub Pages

--- src/main/java/com/caoc/tienda/hibridas/backend/config/SecurityConfig.java
+++ src/main/java/com/caoc/tienda/hibridas/backend/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8100","http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8100","http://localhost:4200, https://caobandoc.github.io/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
